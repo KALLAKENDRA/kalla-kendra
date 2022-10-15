7ca55e9a27da96102c0cf21231a9391fe22178f1
al-jabir <aljabir098@gmail.com>
2022-10-15 08:32:30 +0600

    not sure

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,31 +14,31 @@ const Home = () => {
   return (
     <>
       <div className="relative z-10">
-        <Navbar/>
+        <Navbar />
       </div>
       <div className="top-0 absolute bg-cover">
         <img className="w-screen h-screen" src={bghome} alt="" />
       </div>
-     <div className="flex -mt-10">
-     <div className="relative z-10">
-        <img className="w-60" src={homeBannerP1} alt="" />
+      <div className="flex -mt-10">
+        <div className="relative z-10">
+          <img className="w-60" src={homeBannerP1} alt="" />
+        </div>
+        <div className="relative flex ml-auto">
+          <img className="w-60" src={homeBannerP2} alt="" />
+        </div>
       </div>
-      <div className="relative flex ml-auto">
-        <img className="w-60" src={homeBannerP2} alt="" />
-      </div>
-     </div>
-     <div className="relative -mt-96">
+      <div className="relative -mt-96">
         <div><h1 className="text-6xl text-white font-acme">Welcome! to kalla kendra</h1></div>
         <div><Link to='/startyourjourny'><button className="w-60 h-16 bg-lightRose rounded-full mt-11 text-iridium text-2xl font-aclonica hover:bg-yellow ">World of Artist</button></Link></div>
-     </div>
-     <div className="relative flex justify-evenly mt-44">
+      </div>
+      <div className="relative flex justify-evenly mt-44">
         <img src={speaker1} alt="" />
         <img src={guitarist1} alt="" />
         <img src={Painter1} alt="" />
         <img src={photographer1} alt="" />
         <img src={Singer1} alt="" />
 
-     </div>
+      </div>
 
     </>
   );
