db435a074583ce45838f0beab9aa3b11513a2cac
Shivam06-form <shivambhagwat06071998@gmail.com>
2022-10-29 18:55:19 +0530

    Adding Login  api

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -99,7 +99,7 @@ const Signup = () => {
                       d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     ></path>
                   </svg>
-                  <span>"Temporary prompt"  {tempLogin}.</span>
+                  <span>"Temporary prompt" {tempLogin}.</span>
                 </div>
               </div>
             )}
