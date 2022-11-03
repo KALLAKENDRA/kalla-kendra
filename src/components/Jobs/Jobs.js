3da3a787a79aea3ffd0a159d8d67b70651525b25
Shivam06-form <shivambhagwat06071998@gmail.com>
2022-11-03 16:10:23 +0530

    slider

--- src/components/Jobs/Jobs.js
+++ src/components/Jobs/Jobs.js
@@ -1,92 +1,15 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import locationIcon from "../../Assets/locationIcon.png";
 import searchIcon from "../../Assets/searchicon.png";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
-const interships = [
-  {
-    id: 1,
-    Name: "Graphic Design",
-    Company: "CalciteX Cybarmetics LLp",
-    description: "🏠 Work From Home/ Part time allowed",
-    StartDate: "Immediately",
-    Duration: "3 Months",
-    Stipend: "3000/Per Month",
-    ApplyBT: "11 Aug,2020",
-    type: "Internship",
-  },
-];
 
 const Jobs = () => {
-  const [type, setType] = useState("Internship");
-  const [categories, setCategories] = useState("Remote");
-  const [Jobs, setJobs] = useState();
-
   const background = "https://i.ibb.co/3kvrMsr/Background.png";
   const homeicon = "https://i.ibb.co/d2HTqHm/homeicon.png";
 
-
-  useEffect(() => {
-    console.log(interships.filter((e) => e.type === type));
-    setJobs(interships.filter((e) => e.type === type));
-  }, [type]);
-
-  const renderResult = (Jobs || []).map((job) => (
-    <div
-      key={job.id}
-      className="card w-full bg-base-100 shadow-xl p-5 rounded-none"
-    >
-      <div className="flex items-center justify-between pb-4 flex-wrap">
-        <div className="flex flex-wrap">
-          <h1 className="font-bold text-2xl pb-2">{job.Duration}</h1>
-          <h1 className="font-semibold text-xl">{job.Company}</h1>
-        </div>
-        <div>
-          <button className="btn bg-lightGrey rounded-full pr-4 pl-4 text-black font-bold border-0">
-            {job.type}
-          </button>
-        </div>
-      </div>
-      <div className=" flex items-center justify-start font-bold pt-4 pb-4">
-        <div></div>
-        <div className="flex items-center">
-          <img src={homeicon} className="w-[20px] mr-2" alt="" />
-          <h1>{job.description}</h1>
-        </div>
-      </div>
-
-      <div className="hidden md:flex gap-28 pb-8 w-full justify-between">
-        <div>
-          <h1 className="font-bold">Start date</h1>
-          <h1 className="font-semibold">{job.StartDate}</h1>
-        </div>
-        <div>
-          <h1 className="font-bold">Duration</h1>
-          <h1 className="font-semibold">{job.Duration}</h1>
-        </div>
-        <div>
-          <h1 className="font-bold">Stipend</h1>
-          <h1 className="font-semibold">{job.Stipend}</h1>
-        </div>
-        <div>
-          <h1 className="font-bold">Apply By</h1>
-          <h1 className="font-semibold">{job.ApplyBT}</h1>
-        </div>
-      </div>
-
-      <div className=" flex items-center justify-between">
-        <div>
-          <a href="/" className="underline underline-offset-1 text-vividGreen">
-            View Details
-          </a>
-        </div>
-        <div>
-          <button className="bg-lightRose rounded-full pl-4 pr-4 pb-1 text-black font-bold">
-            Apply
-          </button>
-        </div>
-=======
   return (
     <>
 
@@ -99,137 +22,8 @@ const Jobs = () => {
       </div>
       <div className="relative">
         <Navbar></Navbar>
-
       </div>
-    </div>
-  ));
 
-
-  return (
-    <div className=" overflow-hidden">
-      <img
-        className="w-screen h-screen  bg-cover fixed"
-        src={background}
-        alt=""
-      />
-
-      <div className="relative">
-        <Navbar3 />
-      </div>
-      <div className="w-10/12 m-auto mt-3">
-        <div className="relative flex items-center justify-between mt-4 flex-wrap">
-          <div className="btn-group flex flex-wrap">
-            <button
-              className={`btn  ${
-                type === "PartTime" ? "bg-vividAuburn" : "bg-darkRed"
-              } border-0`}
-              onClick={(e) => {
-                setType("PartTime");
-              }}
-            >
-              Part Time
-            </button>
-            <button
-              className={`btn ${
-                type === "FullTime" ? "bg-vividAuburn" : "bg-darkRed"
-              } border-0`}
-              onClick={(e) => {
-                setType("FullTime");
-              }}
-            >
-              full Time
-            </button>
-            <button
-              className={`btn ${
-                type === "Internship" ? "bg-vividAuburn" : "bg-darkRed"
-              } border-0`}
-              onClick={(e) => {
-                setType("Internship");
-              }}
-            >
-              Internship
-            </button>
-          </div>
-          <div className="btn-group flex flex-wrap">
-            <button className="btn bg-darkRed border-0">
-              Location{" "}
-              <img className="text-white ml-2" src={locationIcon} alt="" />
-            </button>
-          </div>
-          <div className="btn-group flex flex-wrap">
-            <button
-              className={`btn ${
-                categories === "Remote" ? "bg-vividAuburn" : "bg-darkRed"
-              } border-0`}
-              onClick={(e) => {
-                setCategories("Remote");
-              }}
-            >
-              Remote
-            </button>
-            <button
-              className={`btn ${
-                categories === "Inoffice" ? "bg-vividAuburn" : "bg-darkRed"
-              } border-0`}
-              onClick={(e) => {
-                setCategories("Inoffice");
-              }}
-            >
-              In Office
-            </button>
-          </div>
-          <div className="btn-group ">
-            <button className="btn bg-darkRed border-0">Keywords</button>
-          </div>
-          <div className="btn-group flex flex-wrap">
-            <div className="btn bg-darkRed border-0 w-fit ">
-              <input
-                type="search"
-                placeholder="Search"
-                className="bg-transparent outline-none"
-              />
-              <img className="text-white ml-2" src={searchIcon} alt="" />
-            </div>
-          </div>
-        </div>
-
-        <div className="relative flex  justify-between mt-5">
-          <div className="hidden  xl:flex flex-col bg-white p-8 font-semibold justify-between h-4/5 gap-2 w-4/12">
-            <div className=" flex items-center justify-start gap-2">
-              <input
-                type="checkbox"
-                className="w-4 h-4 my-auto checkbox bg-lightGrey rounded-none"
-              />
-              <p>As Per My Preference</p>
-            </div>
-            <div className="form-control w-full ">
-              <label className="label">
-                <span className="label-text">Categories</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here"
-                className="input  w-full  bg-lightGrey rounded-none"
-              />
-            </div>
-            <div className="form-control w-full  pb-4">
-              <label className="label">
-                <span className="label-text">Location</span>
-              </label>
-              <input
-                type="text"
-                placeholder="Type here "
-                className="input  w-full  bg-lightGrey rounded-none"
-              />
-            </div>
-            <div className=" flex flex-col justify-between  h-20">
-              <div className="flex justify-start gap-2 ">
-                <input
-                  type="checkbox"
-                  className="checkbox bg-lightGrey rounded-none w-4 h-4 my-auto"
-                />
-                <p>Work From Home</p>
-=======
       <div className="flex overflow-x-hidden items-center justify-center">
         <div className="relative  grid desktopLg:grid-cols-5 desktopMd:grid-cols-5 desktopSm:grid-cols-5 laptopLg:grid-cols-5 laptopMd:grid-cols-4 laptopSm:grid-cols-2 tablet:grid-cols-1 mobileMd:grid-cols-1 mobileLg:grid-cols-1  gap-12   mt-6 ">
           <div class="btn-group ">
@@ -341,21 +135,25 @@ const Jobs = () => {
                   <h1 className="font-bold">Apply By</h1>
                   <h1 className="font-semibold">11 Aug, 2022</h1>
                 </div>
+              </div>
 
+              <div className=" flex items-center justify-between">
+                <div>
+                  <a
+                    href=""
+                    className="underline underline-offset-1 text-vividGreen"
+                  >
+                    View Details
+                  </a>
+                </div>
+                <div>
+                  <button className="bg-lightRose rounded-full px-2 py-1 text-black">
+                    Apply
+                  </button>
+                </div>
               </div>
-              <div className=" flex items-center justify-start pb-4 gap-2">
-                <input
-                  type="checkbox"
-                  className="checkbox bg-lightGrey rounded-none w-4 h-4 my-auto"
-                />
-                <p>Part Time</p>
-              </div>
-            </div>
-            <div className=" flex items-center justify-end mt-8">
-              <p>Cancel</p>
             </div>
           </div>
-
           <div>
             <div class="card w-full bg-base-100 shadow-xl p-6">
               <div className=" flex items-center justify-between pb-6">
@@ -379,26 +177,44 @@ const Jobs = () => {
                 </div>
               </div>
 
+              <div className="grid grid-cols-4 gap-4 pb-8">
+                <div>
+                  <h1 className="font-bold">Start date</h1>
+                  <h1 className="font-semibold">Immediately</h1>
+                </div>
+                <div>
+                  <h1 className="font-bold">Duration</h1>
+                  <h1 className="font-semibold">3 Months</h1>
+                </div>
+                <div>
+                  <h1 className="font-bold">Stipend</h1>
+                  <h1 className="font-semibold">3000/Per Month</h1>
+                </div>
+                <div>
+                  <h1 className="font-bold">Apply By</h1>
+                  <h1 className="font-semibold">11 Aug, 2022</h1>
+                </div>
+              </div>
 
-          {(Jobs || []).length === 0 && (
-            <div className="font-acme text-4xl w-full flex">
-              <h3 className="text-center m-auto bg-white w-fit p-4 rounded-xl">
-                No results Found
-              </h3>
+              <div className=" flex items-center justify-between">
+                <div>
+                  <a
+                    href=""
+                    className="underline underline-offset-1 text-vividGreen"
+                  >
+                    View Details
+                  </a>
+                </div>
+                <div>
+                  <button className="bg-lightRose rounded-full px-2 py-1 text-black">
+                    Apply
+                  </button>
+                </div>
+              </div>
             </div>
-          )}
-
-          <div className="m-auto flex gap-2  xl:flex flex-col h-full justify-between gap-2 mb-2">
-            {Jobs && renderResult}
-            {Jobs && renderResult}
           </div>
         </div>
       </div>
-
-    </div>
-  );
-};
-
 
 
     </>
@@ -406,7 +222,6 @@ const Jobs = () => {
 
 
 }
-
 
 export default Jobs;
 
--- src/components/Shared/Navbar/Navbar3.js
+++ src/components/Shared/Navbar/Navbar3.js
@@ -1,62 +1,49 @@
-import React, { useState } from "react";
-import { Link, NavLink, useParams } from "react-router-dom";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 import Logo1 from "../../../Assets/svg/Logo1";
 
 const Navbar3 = () => {
-  const [isActive, setIsActive] = useState("");
-
   const activeLink =
     "text-black font-bold bg-aquaHaze  shadow-2xl rounded-full px-3 py-2";
   const normalLink = "font-bold text-white";
-
   return (
-
-    <div className="flex ">
-      <div className="flex justify-between pl-3 text-white w-full flex-wrap xl:w-6/12 ">
-        <div className="">
-          <Link to="/">
-
     <>
       <div className="flex   items-center justify-around">
 
         <div className="relative z-10">
           <Link to="/">
 
-
             <Logo1 />
           </Link>
         </div>
-
-        <Link to="/discover" className=" m-auto">
-          Discover
-        </Link>
-
-        <NavLink to="/livestreams" className="m-auto">
-          Livestreams
-        </NavLink>
-        <div 
-        className="w-fit md:shadow-black shadow-md block bg-darkRed m-auto pl-6 pb-2 pt-2 pr-6 justify-between rounded-2xl flex w-1/6 "
-        >
-          <NavLink to="/jobs" className="m-auto ">
-            Jobs
-          </NavLink>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-5 m-auto"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
+        <div className="flex gap-6">
+          <div>
+            <NavLink
+              to="/discover"
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              Discover
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to="/livestreams"
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              Livestreams
+            </NavLink>
+          </div>
+          <div>
+            <NavLink
+              to="/jobs"
+              className={({ isActive }) => (isActive ? activeLink : normalLink)}
+            >
+              Jobs
+            </NavLink>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
