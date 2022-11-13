c7798750023fca404c38d3a2c2a1dffcaa235b45
Shivam06-form <shivambhagwat06071998@gmail.com>
2022-11-13 21:53:16 +0530

    Slide Show

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -6,8 +6,6 @@ import startjourneyp2 from "../../Assets/startjourneyp2.png";
 import bgCorner1 from "../../Assets/bgCorner1.png";
 import bghome from "../../Assets/bghome.png";
 
-// Shivam Bhagwat
- 
 const images = [
   {
     id: 1,
@@ -37,7 +35,7 @@ const Slideshow = () => {
         <img
           src={isImgm === 0 ? images[isImgm + 2].url : images[isImgm - 1].url}
           alt="shazam"
-          className="w-0 xl:w-56 h-40 my-auto "
+          className="w-0 xl:w-2/12 h-40 my-auto "
         />
         <div className="w-full flex md:flex w-7/12 gap-5">
           <svg
@@ -97,7 +95,7 @@ const Slideshow = () => {
               : images[isImgm + 1].url
           }
           alt="shazam"
-          className="w-0 xl:w-56 h-40 my-auto "
+          className="w-0 xl:w-2/12 h-40 my-auto "
         />
       </div>
       <div className="flex relative w-full">
